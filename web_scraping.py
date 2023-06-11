93e9dcee460bada109756803d20d1af894ac7e59
TitanHaxz <titanhaxz@gmail.com>
2023-06-11 13:18:43 +0300

    Users can input the HTML tag they want to select.

--- web_scraping.py
+++ web_scraping.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 url = input("Enter the URL of the website: ")
 
 # Send an HTTP request to the specified website
-response = requests.get(url)
+# Set verify to False to disable SSL certificate verification (Note: This can be a security risk)
+response = requests.get(url, verify=False)  # Disable SSL certificate verification for testing purposes
 
 # If the request is successful, continue
 if response.status_code == 200:
@@ -15,11 +16,21 @@ if response.status_code == 200:
     # Parse the HTML content
     soup = BeautifulSoup(content, "html.parser")
 
-    # Select the HTML tags you want to manipulate
-    tags = soup.find_all("a")
+    # Get all unique tag names in the HTML
+    all_tags = set(tag.name for tag in soup.find_all())
+
+    # Display all tag names for user reference
+    tag_names_string = ', '.join(f'"{tag}"' for tag in all_tags)
+    print(f"Available tag names: {tag_names_string}")
+
+    # Get the tag from the user
+    tag_name = input("Enter the HTML tag you want to select: ")
+
+    # Select the HTML tags based on user input
+    tags = soup.find_all(tag_name)
 
     # Iterate over the tags
     for tag in tags:
         # Get the tag content or attributes
         print(tag.text)
-        print(tag["href"])
+        print(tag_name+":", tag["href"])
